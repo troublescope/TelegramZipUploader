2e7e81dd39691441176d0e4e12d4721ad7f159d0
tomyprs <tomiprs25@gmail.com>
2023-07-23 22:20:17 +0700

    feat(backup): Add auto remove after succes fix(name): fileName is ugly format, we using new format
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

--- backup.sh
+++ backup.sh
@@ -3,16 +3,15 @@
 # Set the bot token and channel ID
 TOKEN="YOUR_BOT_TOKEN"
 CHANNEL_ID="YOUR_CHANNEL_ID"
-PWD=$(pwd)
 
 # Set the name of the folder to be zipped
 folder="name"
 
 # Set the date and time format
-timestamp=$(date +"%H-%M_%d-%m-%Y")
+timestamp=$(date +"%M.%H-%d.%m.%Y")
 
 # Set the path to the file that stores the counter value
-counter_file="$PWD/backup_count.txt"
+counter_file="backup_count.txt"
 
 # Read the counter value from the file
 if [ -f $counter_file ]; then
@@ -28,7 +27,7 @@ counter=$((counter + 1))
 echo $counter > $counter_file
 
 # Create the zip file name by adding the timestamp and counter
-zipfile="${folder}_${timestamp}_${counter}.zip"
+zipfile="backup-${folder}_${timestamp}.zip"
 
 # Zip the folder with the specified zip file name
 zip -r $zipfile $folder
@@ -38,3 +37,6 @@ caption="File: $zipfile"
 
 # Upload the zip file to the Telegram channel using a bot with a caption
 curl -v -F "chat_id=$CHANNEL_ID" -F document=@$zipfile -F "caption=$caption" https://api.telegram.org/bot$TOKEN/sendDocument
+
+# Delete the zip file after uploading it successfully
+rm $zipfile
